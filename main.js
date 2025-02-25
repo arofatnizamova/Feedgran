8431a10385175fbfe0368c8a2042ffad6a145653
Your Name <you@example.com>
2025-02-25 16:17:46 +0500

    feat: add responsive banner section

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
- 
+$(document).ready(function () {
+    $('.slick-slider').each(function () {
+    let slider = $(this);
+    let options = {
+      prevArrow: slider.parent().find('.slider-prev'),
+      nextArrow: slider.parent().find('.slider-next'),
+      infinite: true,
+    }
+    let extraOptions = {}
+    if (slider.hasClass('banner')) {
+      extraOptions = {
+        slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          arrows: true,
+          autoPlay: true,
+          dots: false,
+        responsive: [
+          
+  ] 
+      }
+    } 
+    slider.slick($.extend({}, extraOptions, options ));
+  })
+})
